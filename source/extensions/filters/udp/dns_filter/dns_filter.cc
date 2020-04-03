52881c74b6c8527a333673fbcb1bd82fe18b4640
Alvin Baptiste <alvinsb@gmail.com>
2020-04-03 03:15:32 +0000

    dns_filter: implement request and response handling
    Signed-off-by: Alvin Baptiste <alvinsb@gmail.com>

--- source/extensions/filters/udp/dns_filter/dns_filter.cc
+++ source/extensions/filters/udp/dns_filter/dns_filter.cc
@@ -1,6 +1,9 @@
 #include "extensions/filters/udp/dns_filter/dns_filter.h"
 
 #include "envoy/network/listener.h"
+#include "envoy/type/matcher/v3/string.pb.h"
+
+#include "common/network/address_impl.h"
 
 namespace Envoy {
 namespace Extensions {
@@ -10,45 +13,217 @@ namespace DnsFilter {
 DnsFilterEnvoyConfig::DnsFilterEnvoyConfig(
     Server::Configuration::ListenerFactoryContext& context,
     const envoy::config::filter::udp::dns_filter::v2alpha::DnsFilterConfig& config)
-    : root_scope_(context.scope()), stats_(generateStats(config.stat_prefix(), root_scope_)) {
+    : root_scope(context.scope()), cluster_manager_(context.clusterManager()),
+      stats_(generateStats(config.stat_prefix(), root_scope)) {
 
   using envoy::config::filter::udp::dns_filter::v2alpha::DnsFilterConfig;
 
-  // store configured data for server context
   const auto& server_config = config.server_config();
 
+  // TODO: Read the external DataSource
   if (server_config.has_inline_dns_table()) {
 
-    const auto& cfg = server_config.inline_dns_table();
-    const size_t entries = cfg.virtual_domains().size();
+    const auto& dns_table = server_config.inline_dns_table();
+    const size_t entries = dns_table.virtual_domains().size();
 
-    // TODO (abaptiste): Check that the domain configured here appears
-    // in the known domains list
     virtual_domains_.reserve(entries);
-    for (const auto& virtual_domain : cfg.virtual_domains()) {
-      DnsAddressList addresses{};
+    for (const auto& virtual_domain : dns_table.virtual_domains()) {
+      AddressConstPtrVec addrs{};
 
       if (virtual_domain.endpoint().has_address_list()) {
         const auto& address_list = virtual_domain.endpoint().address_list().address();
-        addresses.reserve(address_list.size());
+        addrs.reserve(address_list.size());
+        // This will throw an exception if the configured_address string is malformed
         for (const auto& configured_address : address_list) {
-          addresses.push_back(configured_address);
+          const auto ipaddr = Network::Utility::parseInternetAddress(configured_address, 0, true);
+          addrs.push_back(ipaddr);
         }
       }
+      virtual_domains_.emplace(virtual_domain.name(), std::move(addrs));
 
-      virtual_domains_.emplace(virtual_domain.name(), std::move(addresses));
+      uint64_t ttl = virtual_domain.has_answer_ttl()
+                         ? DurationUtil::durationToSeconds(virtual_domain.answer_ttl())
+                         : DefaultResolverTTLs;
+      domain_ttl_.emplace(virtual_domain.name(), ttl);
+    }
+
+    // Add known domains
+    for (const auto& suffix : dns_table.known_suffixes()) {
+      // TODO: We support only suffixes here. Expand this to support other StringMatcher types
+      envoy::type::matcher::v3::StringMatcher matcher;
+      matcher.set_suffix(suffix.suffix());
+      auto matcher_ptr = std::make_unique<Matchers::StringMatcherImpl>(matcher);
+      known_suffixes_.push_back(std::move(matcher_ptr));
     }
   }
+
+  const auto& client_config = config.client_config();
+  forward_queries_ = client_config.forward_query();
+  if (forward_queries_) {
+    const auto& upstream_resolvers = client_config.upstream_resolvers();
+    resolvers_.reserve(upstream_resolvers.size());
+    for (const auto& resolver : upstream_resolvers) {
+      const auto ipaddr = Network::Utility::parseInternetAddress(resolver, 0, true);
+      resolvers_.push_back(std::move(ipaddr));
+    }
+  }
+
+  resolver_timeout_ms_ = std::chrono::milliseconds(
+      PROTOBUF_GET_MS_OR_DEFAULT(client_config, resolver_timeout, DefaultResolverTimeoutMs));
 }
 
 void DnsFilter::onData(Network::UdpRecvData& client_request) {
-  // Handle incoming request and respond with an answer
-  UNREFERENCED_PARAMETER(client_request);
+
+  // Save the connection endpoints so that we can respond
+  local_ = client_request.addresses_.local_;
+  peer_ = client_request.addresses_.peer_;
+
+  // Parse the query, if it fails return an response to the client
+  if (!message_parser_->parseDnsObject(client_request.buffer_)) {
+    sendDnsResponse();
+    return;
+  }
+
+  // Resolve the requested name
+  auto response = getResponseForQuery();
+
+  // We were not able to satisfy the request locally. Return an
+  // empty response to the client
+  if (response == DnsLookupResponseCode::Failure) {
+    sendDnsResponse();
+    return;
+  }
+
+  // Externally resolved. We'll respond to the client when the
+  // external DNS resolution callback returns
+  if (response == DnsLookupResponseCode::External) {
+    return;
+  }
+
+  // We have an answer. Send it to the client
+  sendDnsResponse();
 }
 
-void DnsFilter::onReceiveError(Api::IoError::IoErrorCode error_code) {
-  // Increment error stats
-  UNREFERENCED_PARAMETER(error_code);
+void DnsFilter::sendDnsResponse() {
+
+  // Clear any cruft in the outgoing buffer
+  response_.drain(response_.length());
+
+  // This serializes the generated response to the parse query from the client. If there is a
+  // parsing error or the incoming query is invalid, we will still generate a valid DNS response
+  message_parser_->buildResponseBuffer(response_);
+
+  Network::UdpSendData response_data{local_->ip(), *peer_, response_};
+  listener_.send(response_data);
+}
+
+DnsLookupResponseCode DnsFilter::getResponseForQuery() {
+
+  auto& query_map = message_parser_->getActiveQueryRecords();
+
+  // It appears to be a rare case where we would have more than one query in a single request.
+  // It is allowed by the protocol but not widely supported:
+  //
+  // https://stackoverflow.com/a/4083071
+
+  const uint16_t id = message_parser_->getCurrentQueryId();
+  const auto& query_iter = query_map.find(id);
+
+  if (query_iter == query_map.end()) {
+    ENVOY_LOG_MISC(error, "Unable to find queries for the current transaction id: {}", id);
+  }
+
+  // The number of queries will almost always be one. This governed by the 'questions' field in
+  // the flags. Since the protocol allows for more than one query, we will handle this case.
+  for (const auto& query : query_iter->second) {
+
+    // Try to resolve the query locally. If forwarding the query externally is disabled we will
+    // always attempt to resolve with the configured domains
+    if (isKnownDomain(query->name_) || !config_->forward_queries()) {
+
+      // Determine whether we an answer this query with the static configuration
+      if (resolveViaConfiguredHosts(*query)) {
+        continue;
+      }
+    }
+
+    // TODO: External resolution
+  }
+
+  if (message_parser_->queriesUnanswered(id)) {
+    return DnsLookupResponseCode::Failure;
+  }
+  return DnsLookupResponseCode::Success;
+}
+
+uint32_t DnsFilter::getDomainTTL(const absl::string_view domain) {
+  uint32_t ttl;
+
+  const auto& domain_ttl_config = config_->domain_ttl();
+  const auto& iter = domain_ttl_config.find(domain);
+
+  if (iter == domain_ttl_config.end()) {
+    ttl = static_cast<uint32_t>(DnsFilterEnvoyConfig::DefaultResolverTTLs);
+  } else {
+    ttl = static_cast<uint32_t>(iter->second);
+  }
+
+  return ttl;
+}
+
+bool DnsFilter::isKnownDomain(const absl::string_view domain_name) {
+
+  const auto& known_suffixes = config_->known_suffixes();
+
+  // If we don't have a list of whitelisted domain suffixes, we will resolve the name with an
+  // external DNS server
+  if (known_suffixes.empty()) {
+    ENVOY_LOG(trace, "Known domains list is empty");
+    return false;
+  }
+
+  // TODO: Use a trie to find a match instead of iterating through the list
+  for (auto& suffix : known_suffixes) {
+    if (suffix->match(domain_name)) {
+      return true;
+    }
+  }
+
+  return false;
+}
+
+bool DnsFilter::resolveViaConfiguredHosts(const DnsQueryRecord& query) {
+
+  const auto& domains = config_->domains();
+
+  // TODO: If we have a large ( > 100) domain list, use a binary search.
+  const auto iter = domains.find(query.name_);
+  if (iter == domains.end()) {
+    ENVOY_LOG(debug, "Domain [{}] is not a configured entry", query.name_);
+    return false;
+  }
+
+  const auto& configured_address_list = iter->second;
+  if (configured_address_list.empty()) {
+    ENVOY_LOG(debug, "Domain [{}] address list is empty", query.name_);
+    return false;
+  }
+
+  // Build an answer record from each configured IP address
+  uint64_t hosts_found = 0;
+  for (const auto& configured_address : configured_address_list) {
+    ASSERT(configured_address != nullptr);
+    ENVOY_LOG(debug, "using address {} for domain [{}]",
+              configured_address->ip()->addressAsString(), query.name_);
+    ++hosts_found;
+    const uint32_t ttl = getDomainTTL(query.name_);
+    message_parser_->buildDnsAnswerRecord(query, ttl, configured_address);
+  }
+  return (hosts_found > 0);
+}
+
+void DnsFilter::onReceiveError(Api::IoError::IoErrorCode) {
+  // config_->stats().downstream_sess_rx_errors_.inc();
 }
 
 } // namespace DnsFilter
--- test/extensions/filters/udp/dns_filter/dns_filter_test.cc
+++ test/extensions/filters/udp/dns_filter/dns_filter_test.cc
@@ -3,17 +3,21 @@
 
 #include "common/common/logger.h"
 
-#include "extensions/filters/udp/dns_filter/dns_filter.h"
-
+#include "test/mocks/event/mocks.h"
 #include "test/mocks/server/mocks.h"
 #include "test/test_common/environment.h"
 
+#include "dns_filter_test_utils.h"
 #include "gmock/gmock.h"
 #include "gtest/gtest.h"
 
 using testing::AtLeast;
+using testing::ByMove;
 using testing::InSequence;
+using testing::Mock;
+using testing::Return;
 using testing::ReturnRef;
+using testing::SaveArg;
 
 namespace Envoy {
 namespace Extensions {
@@ -21,26 +25,53 @@ namespace UdpFilters {
 namespace DnsFilter {
 namespace {
 
+Api::IoCallUint64Result makeNoError(uint64_t rc) {
+  auto no_error = Api::ioCallUint64ResultNoError();
+  no_error.rc_ = rc;
+  return no_error;
+}
+
 class DnsFilterTest : public testing::Test {
 public:
   DnsFilterTest()
       : listener_address_(Network::Utility::parseInternetAddressAndPort("127.0.2.1:5353")) {
-
-    Logger::Registry::setLogLevel(spdlog::level::info);
+    // TODO: Consume the log setting from the command line
+    // Logger::Registry::setLogLevel(TestEnvironment::getOptions().logLevel());
+    Logger::Registry::setLogLevel(spdlog::level::trace);
 
     EXPECT_CALL(callbacks_, udpListener()).Times(AtLeast(0));
+    EXPECT_CALL(callbacks_.udp_listener_, send(_))
+        .WillRepeatedly(
+            Invoke([this](const Network::UdpSendData& send_data) -> Api::IoCallUint64Result {
+              response_ptr = std::make_unique<Buffer::OwnedImpl>();
+              response_ptr->move(send_data.buffer_);
+              return makeNoError(response_ptr->length());
+            }));
+
+    EXPECT_CALL(callbacks_.udp_listener_, dispatcher()).WillRepeatedly(ReturnRef(dispatcher_));
   }
 
-  ~DnsFilterTest() override { EXPECT_CALL(callbacks_.udp_listener_, onDestroy()); }
+  ~DnsFilterTest() { EXPECT_CALL(callbacks_.udp_listener_, onDestroy()); }
 
   void setup(const std::string& yaml) {
     envoy::config::filter::udp::dns_filter::v2alpha::DnsFilterConfig config;
     TestUtility::loadFromYamlAndValidate(yaml, config);
     auto store = stats_store_.createScope("dns_scope");
     EXPECT_CALL(listener_factory_, scope()).WillOnce(ReturnRef(*store));
+    EXPECT_CALL(listener_factory_, dispatcher()).Times(AtLeast(0));
+    EXPECT_CALL(listener_factory_, clusterManager()).Times(AtLeast(0));
 
     config_ = std::make_shared<DnsFilterEnvoyConfig>(listener_factory_, config);
     filter_ = std::make_unique<DnsFilter>(callbacks_, config_);
+  }
+
+  void sendQueryFromClient(const std::string& peer_address, const std::string& buffer) {
+    Network::UdpRecvData data;
+    data.addresses_.peer_ = Network::Utility::parseInternetAddressAndPort(peer_address);
+    data.addresses_.local_ = listener_address_;
+    data.buffer_ = std::make_unique<Buffer::OwnedImpl>(buffer);
+    data.receive_time_ = MonotonicTime(std::chrono::seconds(0));
+    filter_->onData(data);
   }
 
   const Network::Address::InstanceConstSharedPtr listener_address_;
@@ -50,10 +81,59 @@ public:
   std::unique_ptr<DnsFilter> filter_;
   Network::MockUdpReadFilterCallbacks callbacks_;
   Stats::IsolatedStoreImpl stats_store_;
-  Runtime::RandomGeneratorImpl rng_;
+  Buffer::InstancePtr response_ptr;
+  DnsMessageParser response_parser_;
 
-  const std::string config_yaml = R"EOF(
+  Event::MockDispatcher dispatcher_;
+
+  // This config has external resolution disabled and is used to verify local lookups. With
+  // external resolution disabled, it eliminates having to setup mocks for the resolver callbacks in
+  // each test.
+  const std::string forward_query_off_config = R"EOF(
 stat_prefix: "my_prefix"
+client_config:
+  forward_query: false
+  upstream_resolvers:
+  - "1.1.1.1"
+  - "8.8.8.8"
+  - "8.8.4.4"
+server_config:
+  inline_dns_table:
+    external_retry_count: 3
+    known_suffixes:
+    - suffix: foo1.com
+    - suffix: foo2.com
+    virtual_domains:
+    - name: "www.foo1.com"
+      endpoint:
+        address_list:
+          address:
+          - 10.0.0.1
+          - 10.0.0.2
+    - name: "www.foo2.com"
+      endpoint:
+        address_list:
+          address:
+          - 2001:8a:c1::2800:7
+          - 2001:8a:c1::2800:8
+          - 2001:8a:c1::2800:9
+    - name: "www.foo3.com"
+      endpoint:
+        address_list:
+          address:
+          - 10.0.3.1
+  )EOF";
+
+  // This config has external resolution enabled. Each test must setup the mock to save and execute
+  // the resolver callback
+  const std::string forward_query_on_config = R"EOF(
+stat_prefix: "my_prefix"
+client_config:
+  forward_query: true
+  upstream_resolvers:
+    - "1.1.1.1"
+    - "8.8.8.8"
+    - "8.8.4.4"
 server_config:
   inline_dns_table:
     external_retry_count: 3
@@ -63,24 +143,132 @@ server_config:
           address_list:
             address:
               - 10.0.0.1
-              - 10.0.0.2
-      - name: "www.foo2.com"
-        endpoint:
-          address_list:
-            address:
-              - 2001:8a:c1::2800:7
-      - name: "www.foo3.com"
-        endpoint:
-          address_list:
-            address:
-              - 10.0.3.1
   )EOF";
 };
 
-TEST_F(DnsFilterTest, TestConfig) {
+TEST_F(DnsFilterTest, InvalidQuery) {
   InSequence s;
 
-  setup(config_yaml);
+  setup(forward_query_off_config);
+
+  sendQueryFromClient("10.0.0.1:1000", "hello");
+
+  ASSERT_TRUE(response_parser_.parseDnsObject(response_ptr));
+
+  ASSERT_EQ(0, Utils::getResponseQuerySize(response_parser_));
+  ASSERT_EQ(0, response_parser_.getAnswers());
+  ASSERT_EQ(3, response_parser_.getQueryResponseCode());
+}
+
+TEST_F(DnsFilterTest, SingleTypeAQuery) {
+  InSequence s;
+
+  setup(forward_query_off_config);
+
+  const std::string domain("www.foo3.com");
+  const std::string query =
+      Utils::buildQueryForDomain(domain, DnsRecordType::A, DnsRecordClass::IN);
+  ASSERT_FALSE(query.empty());
+
+  sendQueryFromClient("10.0.0.1:1000", query);
+
+  ASSERT_TRUE(response_parser_.parseDnsObject(response_ptr));
+
+  ASSERT_EQ(1, Utils::getResponseQuerySize(response_parser_));
+  ASSERT_EQ(1, response_parser_.getAnswers());
+  ASSERT_EQ(0, response_parser_.getQueryResponseCode());
+
+  // Verify that we have an answer record for the queried domain
+  const auto& answers = response_parser_.getAnswerRecords();
+  const auto answer_iter = answers.find(domain);
+  ASSERT_NE(answer_iter, answers.end());
+  ASSERT_EQ(1, answer_iter->second.size());
+  const DnsAnswerRecordPtr& answer = *(answer_iter->second.begin());
+
+  // Verify the address returned
+  const std::list<std::string> expected{"10.0.3.1"};
+  Utils::verifyAddress(expected, answer);
+}
+
+TEST_F(DnsFilterTest, RepeatedTypeAQuery) {
+  InSequence s;
+
+  setup(forward_query_off_config);
+
+  const std::string domain("www.foo3.com");
+
+  for (size_t i = 0; i < 5; i++) {
+    const std::string query =
+        Utils::buildQueryForDomain(domain, DnsRecordType::A, DnsRecordClass::IN);
+    ASSERT_FALSE(query.empty());
+    sendQueryFromClient("10.0.0.1:1000", query);
+
+    response_parser_.reset();
+    ASSERT_TRUE(response_parser_.parseDnsObject(response_ptr));
+
+    ASSERT_EQ(1, Utils::getResponseQuerySize(response_parser_));
+    ASSERT_EQ(1, response_parser_.getAnswers());
+    ASSERT_EQ(0, response_parser_.getQueryResponseCode());
+
+    // Verify that we have an answer record for the queried domain
+    const auto& answers = response_parser_.getAnswerRecords();
+    const auto answer_iter = answers.find(domain);
+    ASSERT_NE(answer_iter, answers.end());
+    ASSERT_EQ(1, answer_iter->second.size());
+    const DnsAnswerRecordPtr& answer = *(answer_iter->second.begin());
+
+    // Verify the address returned
+    std::list<std::string> expected{"10.0.3.1"};
+    Utils::verifyAddress(expected, answer);
+  }
+}
+
+TEST_F(DnsFilterTest, LocalTypeAQueryFail) {
+  InSequence s;
+
+  setup(forward_query_off_config);
+
+  const std::string query =
+      Utils::buildQueryForDomain("www.foo2.com", DnsRecordType::A, DnsRecordClass::IN);
+  ASSERT_FALSE(query.empty());
+
+  sendQueryFromClient("10.0.0.1:1000", query);
+
+  ASSERT_TRUE(response_parser_.parseDnsObject(response_ptr));
+
+  ASSERT_EQ(1, Utils::getResponseQuerySize(response_parser_));
+  ASSERT_EQ(0, response_parser_.getAnswers());
+  ASSERT_EQ(3, response_parser_.getQueryResponseCode());
+}
+
+TEST_F(DnsFilterTest, LocalTypeAAAAQuery) {
+  InSequence s;
+
+  setup(forward_query_off_config);
+
+  std::list<std::string> expected{"2001:8a:c1::2800:7", "2001:8a:c1::2800:8", "2001:8a:c1::2800:9"};
+  const std::string domain("www.foo2.com");
+  const std::string query =
+      Utils::buildQueryForDomain(domain, DnsRecordType::AAAA, DnsRecordClass::IN);
+  ASSERT_FALSE(query.empty());
+
+  sendQueryFromClient("10.0.0.1:1000", query);
+
+  response_parser_.parseDnsObject(response_ptr);
+
+  ASSERT_EQ(1, Utils::getResponseQuerySize(response_parser_));
+  ASSERT_EQ(expected.size(), response_parser_.getAnswers());
+  ASSERT_EQ(0, response_parser_.getQueryResponseCode());
+
+  // Verify that we have an answer record for the queried domain
+  const auto& answers = response_parser_.getAnswerRecords();
+  const auto answer_iter = answers.find(domain);
+  ASSERT_NE(answer_iter, answers.end());
+  ASSERT_EQ(expected.size(), answer_iter->second.size());
+  const DnsAnswerRecordPtr& answer = *(answer_iter->second.begin());
+
+  // Verify the address returned
+  Utils::verifyAddress(expected, answer);
 }
 
 } // namespace
